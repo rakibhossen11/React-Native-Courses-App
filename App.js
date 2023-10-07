d6f6894409600f7794aab82eef602b5d12a71fdd
Rakib Hossen <rakibhossen1660@gmail.com>
2023-10-07 22:44:42 +0600

    add stack navigation

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import HomeScreen from './src/view/screen/HomeScreen';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import CoursesScreen from './src/view/screen/CourseScreen';
 
 const Stack = createStackNavigator();
 
@@ -13,10 +14,37 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{title: null, headerStyle: {elevation:0}}}>
         <Stack.Screen name='HomeScreen' component={HomeScreen} options={{
-          // headerLeft: () =>(
-          //   // <Icon name='sort'  />
-          // )
+          headerLeft: () =>(
+            <Icon name='sort' size={25} style={{margin: 20}} />
+          ),
+          headerRight: () =>(
+            <Image 
+            style={{height: 40, width: 40, marginRight: 20}}
+            source={require('./assets/person.png')}
+            />
+          )
         }} />
+        <Stack.Screen
+          name='Course Screen'
+          component={CoursesScreen}
+          options={({navigation}) =>({
+            headerTransparent: true,
+            headerLeft: () =>(
+              <TouchableOpacity
+              activeOpacity={0.8}
+              onPress={() => navigation.goBack()}>
+                <Icon
+                  name="arrow-back-ios"
+                  size={25}
+                  style={{marginLeft: 20}}
+                />
+              </TouchableOpacity>
+            ),
+            headerRight: () => (
+              <Icon name="more-vert" size={25} style={{marginRight: 20}} />
+            ),
+          }) }
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
